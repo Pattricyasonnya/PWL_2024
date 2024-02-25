c1cbf7c4203f45dd235abaf45acaac14846183e1
Pattricya Sonnya <pattricya13s@gmail.com>
2024-02-25 07:31:36 +0700

    praktikum 2(route, controller dan view)

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return 'Selamat Datang';
+    }
+
+    public function about(){
+        return 'Nama : Pattricya Sonnya Fridyanti '. 'NIM  :2141762060';
+    }
+
+    public function articles($id){
+        return 'Halaman Artikel dengan Id ' .$id;
+    }
+
+}
+    
